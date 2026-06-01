ca33cb4876ead7524adbfbeac1f728cc10a3580c
uladzislau <rusakvlad2@gmail.com>
2026-06-01 11:52:47 +0200

    feat: 501 dashboard shell

---
+++ frontend/src/app/core/services/project.service.ts
@@ -0,0 +1,14 @@
+import { HttpClient } from "@angular/common/http";
+import { inject, Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { Project } from "../models/project.model";
+
+@Injectable({ providedIn: 'root' })
+export class ProjectService {
+    private http = inject(HttpClient);
+    private apiUrl = process.env['NG_APP_JWT__ISSUER'];
+
+    getProjects(): Observable<Project[]> {
+        return this.http.get<Project[]>(this.apiUrl + '/api/project');
+    }
+}
--- frontend/src/app/features/dashboard/dashboard.ts
+++ frontend/src/app/features/dashboard/dashboard.ts
@@ -1,10 +1,21 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { AuthStore } from '../../core/store/auth.store';
+import { ProjectStore } from '../../core/store/project.store';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, CommonModule],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class Dashboard {}
+export class Dashboard implements OnInit {
+  private authStore = inject(AuthStore);
+  private projectStore = inject(ProjectStore);
+
+  ngOnInit(): void {
+    this.projectStore.loadAllProjects();
+  }
+}
